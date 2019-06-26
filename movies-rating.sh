6c533feb50a1f4f54914bdd2e170c2b307feaf4e
Prakash Dahal <prakashdahal@Prakashs-MBP.fios-router.home>
2019-06-26 12:27:24 -0400

    first commit

---
+++ movies-rating.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter the name of the movie?"
+read  movieName
+#This will translate space to %2o so that movie name with space will run in browser
+movieName1=$(echo ${movieName} | tr ' ' '/%20')
+#This will translante all the comma to the new line and grep /10 and delete all other excpt rating
+rating=$(curl -s "http://www.omdbapi.com/?t=${movieName1}&&apikey=6b97b751 " | tr ',' '\n' | grep /10  | tr -d '}]Value:""' | head -1)
+#This will display the rating statement
+echo "${movieName} Rating from Rotten tomato is ${rating}"
